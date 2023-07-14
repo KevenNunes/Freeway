70e3348bbe82f2cbb90f482afa31103e5191437a
Keven Nunes da Silva <103072965+KevenNunes@users.noreply.github.com>
2023-07-14 16:39:27 -0300

    Update carro.js

--- carro.js
+++ carro.js
@@ -1,1 +1,32 @@
+//Código do carro
+let xCarros = [600, 600, 600, 600, 600, 600];
+let yCarros = [40, 96, 150, 210, 270, 318];
+let velocidadeCarros = [7, 5.5, 4.7, 4.0, 8, 6.5];
+let comprimentoCarro = 50;
+let alturaCarro = 40
 
+
+function mostraCarro() {
+  for (let i = 0; i < imagemCarros.length; i++){
+    image(imagemCarros[i], xCarros[i], yCarros[i], comprimentoCarro, alturaCarro);
+  }
+}
+
+
+function movimentaCarro() {
+  for (let i = 0; i < imagemCarros.length; i++){
+    xCarros[i] -= velocidadeCarros[i];
+  }
+}
+
+function voltaPosicaoInicialCarro() {
+  for (let i = 0; i < imagemCarros.length; i++){
+     if (passouTodaATela(xCarros[i])){
+     xCarros[i] = 600;
+    }
+  }
+}
+
+function passouTodaATela(xCarros) {
+  return xCarros < -43;
+}
