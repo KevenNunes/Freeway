975a8e39b42d6e0a706676188edc56eb5cca2713
Keven Nunes da Silva <103072965+KevenNunes@users.noreply.github.com>
2023-07-14 16:40:13 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,19 @@
+function setup() {
+  createCanvas(500, 400);
+  somDaTrilha.loop();
+}
+
+//tela de visualização dos itens
+function draw() {
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialCarro();
+  verificaColisao();
+  incluiPontos();
+  marcaPonto();
+}
+
 
